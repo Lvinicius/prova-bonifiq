d2a019f71589c74c9fa25a41819fdb7aa817572e
Lucas Vinicius <lvinicius123@gmail.com>
2023-10-05 22:34:18 -0300

    Parte 3

--- Services/OrderService.cs
+++ Services/OrderService.cs
@@ -4,22 +4,38 @@ namespace ProvaPub.Services
 {
 	public class OrderService
 	{
-		public async Task<Order> PayOrder(string paymentMethod, decimal paymentValue, int customerId)
-		{
-			if (paymentMethod == "pix")
-			{
-				//Faz pagamento...
-			}
-			else if (paymentMethod == "creditcard")
-			{
-				//Faz pagamento...
-			}
-			else if (paymentMethod == "paypal")
-			{
-				//Faz pagamento...
-			}
+        private readonly Dictionary<string, Func<Task>> paymentMethods = new Dictionary<string, Func<Task>>
+        {
+            ["pix"] =
+            {
+                // Faz Pagamento
+            },
+            ["creditcard"] = 
+            {
+                //Faz pagamento...
+            },
+            ["paypal"] = 
+            {
+                //Faz pagamento...
+            },
+            ["cash"] =
+            {
+                //Faz pagamento...
+            }
+        };
 
-			return await Task.FromResult( new Order()
+        public async Task<Order> PayOrder(string paymentMethod, decimal paymentValue, int customerId)
+		{
+            if (paymentMethods.TryGetValue(paymentMethod, out var paymentFunction))
+            {
+                await paymentFunction.Invoke();
+            }
+            else
+            {
+                Console.WriteLine("Método de pagamento não suportado.");
+            }
+
+            return await Task.FromResult( new Order()
 			{
 				Value = paymentValue
 			});
