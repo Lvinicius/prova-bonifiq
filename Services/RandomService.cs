68d0ec8219482352d6d72185cf512ef7bb55fff9
Lucas Vinicius <lvinicius123@gmail.com>
2023-10-05 21:29:11 -0300

    Ajuste Parte I

--- Services/RandomService.cs
+++ Services/RandomService.cs
@@ -2,11 +2,7 @@ namespace ProvaPub.Services
 {
 	public class RandomService
 	{
-		int seed;
-		public RandomService()
-		{
-			seed = Guid.NewGuid().GetHashCode();
-		}
+		
 		public int GetRandom()
 		{
 			return new Random().Next(100);
